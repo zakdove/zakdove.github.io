6d73888f66d537739425519900244d72753f0ffa
zakdove <zakdove05@gmail.com>
2020-10-30 13:11:28 +0000

    save message

--- projects/platformer/js/init/platform.js
+++ projects/platformer/js/init/platform.js
@@ -47,7 +47,8 @@ scaleY: OPTIONAL The scale factor on the y-axis, this value will stretch the pla
         createPlatform(400, 460);    // normal platform
         createPlatform(300, 200, 0.3);  // small horizontal platform (30% the normal width)
         createPlatform(500, 500, 0.3, 10)  // tall vertical wall (30% the normal width and 10x the height)
-        
+        createPlatform(500, 300, 0.3, 10) // jump on vertical wall
+        createPlatform(500, 300, 0.3, 10) // jump left off of vertical wall
         // ALL YOUR CODE GOES ABOVE HERE ///////////////////////////////////////
         ////////////////////////////////////////////////////////////////////////
     }
